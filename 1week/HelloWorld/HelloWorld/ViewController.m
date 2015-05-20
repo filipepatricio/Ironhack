ad59b6d163b8f4a10da3303c85fee04648ebf6c8
Filipe Patrício <facpatricio@gmail.com>
2015-05-20 12:04:49 +0200

    Added projects on 1st week

---
+++ 1week/HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Filipe Patrício on 04/05/15.
+//  Copyright (c) 2015 Filipe Patrício. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello World!!");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
